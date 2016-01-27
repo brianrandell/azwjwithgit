17a77bcf26b853d50e294320a2236577647f3939
Brian Randell (MCW) <brianr@mcwtech.com>
2016-01-26 21:54:36 -0800

    Add code that uses libgit2sharp and then fails under Azure WebJob.

--- azwjtest/libgit2sharpwebjob/Functions.cs
+++ azwjtest/libgit2sharpwebjob/Functions.cs
@@ -1,10 +1,7 @@
-﻿using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.IO;
 using Microsoft.Azure.WebJobs;
 using LibGit2Sharp;
+using System;
 
 namespace libgit2sharpwebjob
 {
@@ -17,13 +14,92 @@ namespace libgit2sharpwebjob
     {
       log.WriteLine("Function is invoked with value={0}", value);
       message = value.ToString();
+      RunLibGit2SharpCode(log);
       log.WriteLine("Following message will be written on the Queue={0}", message);
     }
 
     private static void RunLibGit2SharpCode(TextWriter log)
     {
-      Repository repo = null;
-      log.WriteLine("Repo is nuull");
+      // TODO: enter your VSTS alternate credentials
+      string userName = "";
+      string password = "";
+
+      // TODO: enter your VSTS account name
+      string accountRoot = "yourroot";
+
+      // TODO: enter the name of a clean VSTS team project 
+      // with an emptry default Git repo
+      string teamProjectName = "yourteamproject";
+
+      try
+      {
+        var co = new CloneOptions();
+        co.CredentialsProvider = (_url, _user, _cred) =>
+          new UsernamePasswordCredentials
+          {
+            Username = userName,
+            Password = password
+          };
+
+        string reposRoot = Path.Combine(
+          Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+          "repos");
+        if (Directory.Exists(reposRoot))
+        {
+          // directory exists, delete
+          log.WriteLine("Cleaning reposRoot: " + reposRoot);
+          DeleteReadOnlyDirectory(reposRoot);
+        }
+        string repoRoot = Path.Combine(reposRoot, teamProjectName);
+
+        string accountUrl = string.Format("https://{0}.visualstudio.com/DefaultCollection", accountRoot);
+
+        Repository.Clone(accountUrl + "/_git/" + teamProjectName, repoRoot, co);
+        log.WriteLine("Clone Done!");
+
+        using (Repository repo = new Repository(repoRoot))
+        {
+          log.WriteLine("Repo aquired");
+
+          //// Write file
+          string newFilePath = System.IO.Path.Combine(repoRoot, "test.txt");
+          System.IO.File.WriteAllText(newFilePath, "Some great text.");
+          log.WriteLine("New text file written");
+
+          log.WriteLine("repo.RetrieveStatus()");
+          var status = repo.RetrieveStatus();
+          foreach (var item in status)
+          {
+            if (item.State == FileStatus.Untracked)
+            {
+              // *** Warning ***
+              // this line of code causes the Azure Web Job to blow up
+              repo.Index.Add(item.FilePath);
+              log.WriteLine(item.FilePath + " added to index");
+            }
+          }
+        }
+      }
+      catch (Exception ex)
+      {
+        log.WriteLine("Failure in LoadCodeIntoGitRepo");
+        log.WriteLine(ex.ToString());
+      }
+    }
+
+    private static void DeleteReadOnlyDirectory(string directory)
+    {
+      foreach (var subdirectory in Directory.EnumerateDirectories(directory))
+      {
+        DeleteReadOnlyDirectory(subdirectory);
+      }
+      foreach (var fileName in Directory.EnumerateFiles(directory))
+      {
+        var fileInfo = new FileInfo(fileName);
+        fileInfo.Attributes = FileAttributes.Normal;
+        fileInfo.Delete();
+      }
+      Directory.Delete(directory);
     }
   }
 }
